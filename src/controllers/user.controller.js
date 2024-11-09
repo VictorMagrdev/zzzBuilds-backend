d4c9447d530e189311fd78a173bd554a6c707a15
VictorMagrdev <victorakiladev@gmail.com>
2024-11-09 16:21:22 -0500

    user to username

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,114 +1,114 @@
 import { pool } from "../libs/db.js";
-
 import { errorHandler } from "../helpers/errorHandler.js";
 import jwt from 'jsonwebtoken';
 
+// Obtener usuario por ID
 export const getUserById = (req, res) => {
   const { id_user } = req.params;
 
   pool.query(`
-    SELECT id_user, fullname, user, email, state_id
+    SELECT id_user, fullname, username, email, state_id
     FROM zenleszz.usuarios
     WHERE id_user = ?
   `, [id_user])
-  .then((data) => {
-    const user = data.rows;
-    if (!user) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
-    }
+    .then((data) => {
+      const user = data.rows;
+      if (!user || user.length === 0) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
 
-    res.json(user);
-  })
-  .catch(error => {
-    errorHandler(res, 500, "Error al obtener la información del usuario", error);
-  });
+      res.json(user);
+    })
+    .catch(error => {
+      errorHandler(res, 500, "Error al obtener la información del usuario", error);
+    });
 };
 
-
+// Registrar usuario
 export const registerUser = async (req, res) => {
-  const { name, user, email, password, confirmPassword } = req.body;
+  const { name, username, email, password, confirmPassword } = req.body;
   if (password !== confirmPassword) {
     return res.status(400).json({ error: 'Las contraseñas no coinciden' });
   }
 
   pool.query(`
-    SELECT * FROM zenleszz.usuarios WHERE user = ? OR email = ?
-  `, [user, email])
-  .then(([existingUser]) => {
+    SELECT * FROM zenleszz.usuarios WHERE username = ? OR email = ?
+  `, [username, email])
+    .then(([existingUser]) => {
+      if (existingUser.length > 0) {
+        return res.status(400).json({ error: 'El correo o el nombre de usuario ya están registrados' });
+      }
 
-    if (existingUser.length > 0) {
-      return res.status(400).json({ error: 'El correo o el nombre de usuario ya están registrados'});
-    }
-
-    pool.query(`
-      INSERT INTO zenleszz.usuarios (fullname, user, email, password, state_id)
-      VALUES (?, ?, ?, ?, ?)
-    `, [name, user, email, password, 1])
-    .then(() => {
-      res.status(201).json({ message: 'Usuario registrado exitosamente'});
+      pool.query(`
+        INSERT INTO zenleszz.usuarios (fullname, username, email, password, state_id)
+        VALUES (?, ?, ?, ?, ?)
+      `, [name, username, email, password, 1])
+        .then(() => {
+          res.status(201).json({ message: 'Usuario registrado exitosamente' });
+        })
+        .catch(error => {
+          errorHandler(res, 500, "Error al registrar el usuario", error);
+        });
     })
     .catch(error => {
-      errorHandler(res, 500, "Error al registrar el usuario", error);
+      errorHandler(res, 500, "Error al verificar el email o nombre de usuario", error);
     });
-  })
-  .catch(error => {
-    errorHandler(res, 500, "Error al verificar el email o nombre de usuario", error);
-  });
 };
 
-
+// Iniciar sesión
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   pool.query(`
     SELECT * FROM zenleszz.usuarios WHERE email = ?
   `, [email])
-  .then((data) => {
-    const user = data.rows;
+    .then((data) => {
+      const user = data.rows;
 
-    if (!user[0].email) {
-      return res.status(400).json({ error: 'Correo incorrecto' });
-    }
+      if (!user[0]?.email) {
+        return res.status(400).json({ error: 'Correo incorrecto' });
+      }
 
-    if (password !== user[0].password) {
-      return res.status(400).json({ error: 'Contraseña incorrecta' });
-    }
+      if (password !== user[0].password) {
+        return res.status(400).json({ error: 'Contraseña incorrecta' });
+      }
 
-    const token = jwt.sign({ userId: user[0].id_user }, 'secretKey', { expiresIn: '1h' });
+      const token = jwt.sign({ userId: user[0].id_user }, 'secretKey', { expiresIn: '1h' });
 
-    res.json({
-      token,
-      user: { id: user[0].id_user, fullname: user[0].fullname, user: user[0].user, email: user[0].email }
+      res.json({
+        token,
+        user: { id: user[0].id_user, fullname: user[0].fullname, username: user[0].username, email: user[0].email }
+      });
+    })
+    .catch(error => {
+      errorHandler(res, 500, "Error al iniciar sesión", error);
     });
-  })
-  .catch(error => {
-    errorHandler(res, 500, "Error al iniciar sesión", error);
-  });
 };
 
-
+// Obtener perfil del usuario
 export const getUserProfile = async (req, res) => {
   const userId = req.userId;
 
   pool.query(`
-    SELECT user, img_profile
+    SELECT username, img_profile
     FROM zenleszz.usuarios
     WHERE id_user = ?
   `, [userId])
-  .then((data) => {
-    const user = data.rows;
+    .then((data) => {
+      const user = data.rows;
 
-    if (!user || user.length === 0) {
-      return res.status(404).json({ error: 'Usuario no encontrado' });
-    }
+      if (!user || user.length === 0) {
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+      }
 
-    res.json(user[0]);
-  })
-  .catch(error => {
-    errorHandler(res, 500, "Error al obtener los datos del usuario", error);
-  });
+      res.json(user[0]);
+    })
+    .catch(error => {
+      errorHandler(res, 500, "Error al obtener los datos del usuario", error);
+    });
 };
 
+// Verificar token
 export const verifyToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
 
@@ -117,11 +117,10 @@ export const verifyToken = (req, res, next) => {
   }
 
   jwt.verify(token, 'secretKey', (err, decoded) => {
-    if (err) {;
+    if (err) {
       return res.status(401).json({ error: 'Token inválido' });
     }
     req.userId = decoded.userId;
     next();
   });
 };
-
